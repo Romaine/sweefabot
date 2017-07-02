940de93d5978a96c60dbec6f4e0035c77d94e468
BuildTools <RomaineRC@gmail.com>
2017-07-03 00:14:21 +0100

    Added TTS and Youtube command.

--- sweefabot.py
+++ sweefabot.py
@@ -2,39 +2,45 @@ import discord
 import asyncio
 
 from auth import token
+from discord.ext import commands
 
-client = discord.Client()
+from mlgvoice import download
 
 
-async def work():
-    print("done")
-    channel = client.get_channel('314489891859726347')
-    voice = await client.join_voice_channel(channel)
-    player = voice.create_ffmpeg_player('cool.mp3')
+description = "The most MLG bot in the world"
+bot = commands.Bot(command_prefix='/', description=description)
+
+
+@bot.command()
+async def say(text):
+    mp3 = download(
+        text=text,
+        engine=6,
+        language=1,
+        voice=5,
+    )
+    player = bot.voice.create_ffmpeg_player(mp3)
     player.start()
 
 
-@client.event
+@bot.command()
+async def youtube(*args):
+    if args[0] == "stop":
+        bot.player.stop()
+    else:
+        link = args[0]
+        bot.player = await bot.voice.create_ytdl_player(link)
+        bot.player.start()
+
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
-    await work()
 
+    channel = bot.get_channel('314489891859726347')
+    bot.voice = await bot.join_voice_channel(channel)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
-client.run(token)
+bot.run(token)
