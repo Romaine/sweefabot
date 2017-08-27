d4a9804712fca7237c11a2b1a11cee6c7199e5cc
BuildTools <RomaineRC@gmail.com>
2017-08-27 10:25:41 +0100

    Changed Load opus call.

--- sweefabot.py
+++ sweefabot.py
@@ -62,7 +62,8 @@ async def on_ready():
     print('------')
 
     if not discord.opus.is_loaded():
-        discord.opus.load_opus(find_library("opus"))
+        #discord.opus.load_opus(find_library("opus"))
+        discord.opus.load_opus()
 
     channel = bot.get_channel('314489891859726347')
     bot.voice = await bot.join_voice_channel(channel)
